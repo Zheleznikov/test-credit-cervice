8f50b3df07e1d4540b9b8ed29eaf6388df7ff732
Serg Zhe <polwen@yandex.ru>
2022-05-02 01:49:54 +0300

    add local host to test on heroku bench

--- conftest.py
+++ conftest.py
@@ -1,13 +1,15 @@
 import pytest
 
-from consts.hosts import local_host
+from consts.hosts import local_host, heroku_host
 
 
 def pytest_addoption(parser):
-    parser.addoption("--bench", action="store", default="local")
+    parser.addoption("--bench", action="store", default="heroku")
 
 
 @pytest.fixture(scope="function")
 def bench(request):
     if request.config.getoption("bench") == "local":
         return local_host
+    elif request.config.getoption("bench") == "heroku":
+        return heroku_host
--- consts/hosts.py
+++ consts/hosts.py
@@ -1,2 +1,3 @@
 local_host = 'http://localhost:8080/'
 check_info = 'check-info-about-credit'
+heroku_host = 'https://floating-hollows-03946.herokuapp.com/'
